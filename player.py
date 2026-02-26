b6b45c64b38536cc4778b96821e3ceae81581b39
Hugo Facini <news.word.game@gmail.com>
2026-02-26 16:50:52 -0300

    Add player and random AI move logic

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import math
 import random
+import game
 
 class Player:
 	def __init__(self, letter): #letter is x or o
@@ -13,11 +14,27 @@ class RandomComputerPlayer(Player):
 		super().__init__(letter)
 
 	def get_move(self, game):
-		pass
+		#get a random valid spot for our next move
+		square = random.choice(game.available_moves())
+		return square
 
 class HumanPlayer(Player):
 	def __init__(self, letter):
 		super().__init__(letter)
 
 	def get_move(self, game):
-		pass
+		valid_square = False
+		val = None
+		while not valid_square:
+			square = input(self.letter + '\'s turn. Input move (0-8): ')
+			# we are going to check that this is a correct value by trying to cast 
+			# it to an integer, and if it's not, we say it's invalid
+			# if that spot is not available on the board, we also say it's invalid
+			try:
+				val = int(square)
+				if val not in game.available_moves():
+					raise ValueError
+				valid_square = True #if these are successful, then yay!
+			except ValueError:
+					print('Invalid square. Try again.')99
+		return val
